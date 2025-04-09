9948ed2daaab7557fb4cf87f8b1620cad25868c0
Hatim Ouaadi <hatim.ouaadi@epitech.eu>
2025-04-09 14:43:17 +0200

    [ADD] coding style DFS

--- src/algo.c
+++ src/algo.c
@@ -6,8 +6,15 @@
 */
 
 #include "../lib/my/my.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int algo(char **data)
 {
-    
+    int fd = open(data[1], O_RDONLY);
+    char *buff;
+
+    read(fd, buff, sizeof(buff));
+    printf("%s", buff);
 }
