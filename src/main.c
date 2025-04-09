7a061114ae25452fa865582c1b25186134eb08c4
Theo <theo.vitte@epitech.eu>
2025-04-09 15:31:54 +0200

    gestion d erreur WIP

--- src/main.c
+++ src/main.c
@@ -14,18 +14,19 @@ int main(int argc, char **argv)
     int fd;
     char **data = malloc(sizeof(char *)* argc);
 
-    if (argc < 3)
+    if (argc != 3 && argc != 5)
         return 84;
     if (strcmp("-f", argv[1]) != 0)
         return 84;
     fd = open(argv[2], O_RDONLY);
     if (fd == -1)
         return 84;
-    if (argc == 5){
+   if (argc == 5){
         if (strcmp("-s", argv[3]) != 0)
             return 84;
         if (is_digit(argv[4]) != 1)
            return 84;
     }
-    printf("allo salem %s\n", argv[0]);
+    data = &argv[1];
+    printf("allo salem %s\n", data[0]);
 }
